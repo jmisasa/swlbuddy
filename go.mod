2c46b2980f7a70fd165acdc7e4960169b819eac1
jmisasa <jmisasa@gmail.com>
2022-03-13 05:07:41 +0100

    add telnet conn wip

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/jmisasa/swlbuddy
 
 go 1.17
 
-require github.com/mattn/go-gtk v0.0.0-20191030024613-af2e013261f5
+require (
+	github.com/mattn/go-gtk v0.0.0-20191030024613-af2e013261f5
+	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e
+)
 
 require (
 	github.com/mattn/go-pointer v0.0.1 // indirect
+	github.com/reiver/go-oi v1.0.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/mattn/go-gtk/glib"
 	"github.com/mattn/go-gtk/gtk"
+	"github.com/reiver/go-telnet"
 	"os"
 )
 
@@ -14,11 +15,32 @@ func main() {
 	window.SetPosition(gtk.WIN_POS_CENTER)
 	window.SetTitle("SWLBuddy")
 	window.SetIconName("gtk-dialog-info")
-	
+
 	window.Connect("destroy", func(ctx *glib.CallbackContext) {
 		fmt.Println("got destroy!", ctx.Data().(string))
 		gtk.MainQuit()
 	}, "foo")
+
+	conn, err := telnet.DialTo("localhost:7356")
+
+	if err != nil {
+		panic("Couldn't connect to RigControl server")
+	}
+
+	_, err = conn.Write([]byte("f"))
+
+	if err != nil {
+		panic("Couldn't write to server")
+	}
+
+	//	b := make([]byte, 4096)
+	//	n, err := conn.Read(b)
+
+	if err != nil {
+		panic("Couldn't read from server")
+	}
+
+	//	fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
 
 	window.SetSizeRequest(600, 600)
 	window.ShowAll()
