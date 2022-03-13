1346798457c4ca1615b12f21dadc4e01e6a3610c
jmisasa <jmisasa@gmail.com>
2022-03-13 16:27:58 +0100

    wip display frequency in gui

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/mattn/go-gtk/gdkpixbuf"
 	"github.com/mattn/go-gtk/glib"
 	"github.com/mattn/go-gtk/gtk"
 	"github.com/reiver/go-telnet"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
@@ -29,15 +31,66 @@ func main() {
 		panic("Couldn't connect to RigControl server")
 	}
 
-	err, frequency := sendCommand(conn, "f")
+	freqLabel := gtk.NewLabel("Frequency")
 
-	if err != nil {
-		fmt.Printf("Couldn't run command: %v", err)
-		panic("couldn't couldn't")
-	}
+	ticker := time.NewTicker(1 * time.Second)
+	quit := make(chan struct{})
 
-	fmt.Printf("err = %v string = %s\n", err, frequency)
+	go func(conn *telnet.Conn, label *gtk.Label) {
+		for {
+			select {
+			case <-ticker.C:
+				err, frequency := sendCommand(conn, "f")
 
+				if err != nil {
+					panic("error")
+				}
+
+				fmt.Println("Freq %s", frequency)
+				label.SetText(frequency)
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}(conn, freqLabel)
+
+	vpaned := gtk.NewVPaned()
+
+	store := gtk.NewListStore(glib.G_TYPE_STRING, glib.G_TYPE_BOOL, gdkpixbuf.GetType())
+	treeview := gtk.NewTreeView()
+	vpaned.Pack1(freqLabel, false, true)
+	vpaned.Pack2(treeview, true, false)
+
+	treeview.SetModel(store)
+	treeview.AppendColumn(gtk.NewTreeViewColumnWithAttributes("Station", gtk.NewCellRendererText(), "text", 0))
+	treeview.AppendColumn(gtk.NewTreeViewColumnWithAttributes("Country", gtk.NewCellRendererText(), "text", 1))
+	treeview.AppendColumn(gtk.NewTreeViewColumnWithAttributes("Language", gtk.NewCellRendererText(), "text", 2))
+
+	/*
+		n := 0
+		gtk.StockListIDs().ForEach(func(d unsafe.Pointer, v interface{}) {
+			id := glib.GPtrToString(d)
+			var iter gtk.TreeIter
+			store.Append(&iter)
+			store.Set(&iter,
+				0, id,
+				1, (n == 1),
+				2, gtk.NewImage().RenderIcon(id, gtk.ICON_SIZE_SMALL_TOOLBAR, "").GPixbuf,
+			)
+			n = 1 - n
+		})
+	*/
+	notebook := gtk.NewNotebook()
+
+	tabLabel := "By frequency"
+	notebook.AppendPage(vpaned, gtk.NewLabel("By frequency"))
+
+	tabLabel = "Currently transmitting"
+	currentlyTransmitting := gtk.NewFrame(tabLabel)
+	notebook.AppendPage(currentlyTransmitting, gtk.NewLabel(tabLabel))
+
+	window.Add(notebook)
 	window.SetSizeRequest(600, 600)
 	window.ShowAll()
 	gtk.Main()
@@ -46,6 +99,8 @@ func main() {
 func sendCommand(conn *telnet.Conn, command string) (error, string) {
 	_, err := conn.Write([]byte(command))
 	_, err = conn.Write([]byte("\n"))
+
+	fmt.Println("command sent\n")
 
 	if err != nil {
 		return err, ""
@@ -62,6 +117,8 @@ func readLine(reader io.Reader) (error, string) {
 
 	for {
 		_, err := reader.Read(b)
+
+		fmt.Printf("read byte %v\n", b)
 
 		if bytes.Equal(b, []byte{10}) {
 			break
